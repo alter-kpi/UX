b5abf63053fba745f39ac774bf44b3d6c5871662
Alter KPi <fm@alter-kpi.com>
2025-07-07 15:19:39 +0200

    Update app.py

--- app.py
+++ app.py
@@ -186,7 +186,7 @@ if uploaded_file:
     except Exception as e:
         st.error(f"Une erreur est survenue : {str(e)}")
 
-
+st.markdown("---")
 st.markdown("Télécharger le template Excel des résultats à charger dans cette application :")
 
 with open("template_sus.xlsx", "rb") as f:
