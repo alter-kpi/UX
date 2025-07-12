c9fb3f24b27a314835ce8f3ce0a7ab64ae9c94bc
Alter KPi <fm@alter-kpi.com>
2025-07-12 22:04:33 +0200

    Update app.py

--- app.py
+++ app.py
@@ -9,48 +9,6 @@ import tempfile
 
 st.set_page_config(page_title="AlterUX - Analyse SUS", layout="centered")
 
-#Paramètres des couleurs
-
-zone_colors = ["#d9534f", "#f0ad4e", "#f7ec13", "#5bc0de", "#5cb85c", "#3c763d"]
-zones = [
-    (0, 25, zone_colors[0], "Pire imaginable"),
-    (25, 39, zone_colors[1], "Mauvais"),
-    (39, 52, zone_colors[2], "Acceptable"),
-    (52, 73, zone_colors[3], "Bon"),
-    (73, 86, zone_colors[4], "Excellent"),
-    (86, 100, zone_colors[5], "Meilleur imaginable")
-]
-
-# Menu latéral
-st.sidebar.title(labels[lang]["sidebar_title"])
-
-# Choix de la langue
-lang = st.sidebar.selectbox(
-    labels[lang]["sidebar_language"],
-    options=["Français", "English"],
-    index=0
-)
-
-
-
-zones = [
-    (0, 25, zone_colors[0], "Pire imaginable"),
-    (25, 39, zone_colors[1], "Mauvais"),
-    (39, 52, zone_colors[2], "Acceptable"),
-    (52, 73, zone_colors[3], "Bon"),
-    (73, 86, zone_colors[4], "Excellent"),
-    (86, 100, zone_colors[5], "Meilleur imaginable")
-]
-
-# Menu latéral
-st.sidebar.title(labels[lang]["sidebar_title"])
-
-# Choix de la langue
-lang = st.sidebar.selectbox(
-    labels[lang]["sidebar_language"],
-    options=["Français", "English"],
-    index=0
-)
 # Dictionnaire de traduction complet
 labels = {
     "Français": {
@@ -124,6 +82,50 @@ labels = {
         "questionnaire_choices": ["SUS", "Other (coming soon)"]
     }
 }
+
+
+#Paramètres des couleurs
+
+zone_colors = ["#d9534f", "#f0ad4e", "#f7ec13", "#5bc0de", "#5cb85c", "#3c763d"]
+zones = [
+    (0, 25, zone_colors[0], "Pire imaginable"),
+    (25, 39, zone_colors[1], "Mauvais"),
+    (39, 52, zone_colors[2], "Acceptable"),
+    (52, 73, zone_colors[3], "Bon"),
+    (73, 86, zone_colors[4], "Excellent"),
+    (86, 100, zone_colors[5], "Meilleur imaginable")
+]
+
+# Menu latéral
+st.sidebar.title(labels[lang]["sidebar_title"])
+
+# Choix de la langue
+lang = st.sidebar.selectbox(
+    labels[lang]["sidebar_language"],
+    options=["Français", "English"],
+    index=0
+)
+
+
+
+zones = [
+    (0, 25, zone_colors[0], "Pire imaginable"),
+    (25, 39, zone_colors[1], "Mauvais"),
+    (39, 52, zone_colors[2], "Acceptable"),
+    (52, 73, zone_colors[3], "Bon"),
+    (73, 86, zone_colors[4], "Excellent"),
+    (86, 100, zone_colors[5], "Meilleur imaginable")
+]
+
+# Menu latéral
+st.sidebar.title(labels[lang]["sidebar_title"])
+
+# Choix de la langue
+lang = st.sidebar.selectbox(
+    labels[lang]["sidebar_language"],
+    options=["Français", "English"],
+    index=0
+)
 
 # Définition dynamique des zones
 zone_colors = ["#d9534f", "#f0ad4e", "#f7ec13", "#5bc0de", "#5cb85c", "#3c763d"]
