9be4b0e9e2a99a335b24bcddf322a01eb53f384b
Alter KPi <fm@alter-kpi.com>
2025-07-12 21:31:47 +0200

    Update app.py

--- app.py
+++ app.py
@@ -81,8 +81,7 @@ sus_questions = {
 
 st.set_page_config(page_title="AlterUX - Analyse SUS", layout="centered")
 
-#st.title("📊 Analyse de questionnaire SUS")
-st.title("Willie ❤️🩷🧡💛💚💙 💝💗💓💌")
+st.title("📊 Analyse de questionnaire SUS")
 st.markdown("Chargez un fichier **Excel (.xlsx)** contenant une ligne d'en-tête avec les colonnes **Question1** à **Question10**.")
 
 uploaded_file = st.file_uploader("Charger le fichier Excel", type=["xlsx"])
