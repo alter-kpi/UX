ae68af35fb36c335795ebc7cc4d0230a17020739
Alter KPi <fm@alter-kpi.com>
2025-07-15 12:07:23 +0200

    Update app.py

--- app.py
+++ app.py
@@ -515,7 +515,13 @@ if uploaded_file:
 
                 if stats_df is not None:
                     add_stats_table(pdf, stats_df, "Statistiques par question")
-                
+                if question_stats_df is not None:
+                    add_stats_table(pdf, question_stats_df, "Statistiques par question")
+            
+                try:
+                    return pdf.output(dest='S').encode('latin1')
+                except UnicodeEncodeError:
+                    return None
 
 
             # Appel depuis Streamlit
